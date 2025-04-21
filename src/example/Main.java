4c4d94f04153d120e934a821599d4a9ec0249c8a
Gamer-art9044 <131509007+Gamer-art9044@users.noreply.github.com>
2025-04-21 13:39:51 +0400

    Update Main.java

--- src/example/Main.java
+++ src/example/Main.java
@@ -62,7 +62,7 @@ public class Main{
         Vars.content.createBaseContent();  // Загрузка базового контента
         Vars.world = new World(); // Инициализация игрового мира
         net = net2;
-        Vars.netClient = new BNetClient();
+        Vars.netClient = new NetClient();
         logic = new Logic();
         Groups.init();
 
